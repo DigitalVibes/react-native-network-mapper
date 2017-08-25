3717c7a33a77a00ece0df569fce5a0a3bb5c89b0
pavan-08 <pavanchhatpar@gmail.com>
2017-08-25 13:49:02 +0530

    Trial for iOS network mapper

--- ios/RNNetworkMapper.h
+++ ios/RNNetworkMapper.h
@@ -5,7 +5,13 @@
 #import <React/RCTBridgeModule.h>
 #endif
 
-@interface RNNetworkMapper : NSObject <RCTBridgeModule>
+#import "MMLanScanner.h"
+#import "MMDevice.h"
 
+@interface RNNetworkMapper : NSObject <RCTBridgeModule, MMLANScannerDelegate>
+@property(nonatomic, weak)id: <RNNetworkMapperDelegate> delegate;
+@property(nonatomic, strong)MMLANScanner *lanScanner;
+@property(nonatomic, assign, readwrite) float progressValue;
+@property(nonatomic, assign, readwrite) BOOL isScanRunning;
 @end
   
--- ios/RNNetworkMapper.m
+++ ios/RNNetworkMapper.m
@@ -1,25 +1,79 @@
 
 #import "RNNetworkMapper.h"
 
-@implementation RNNetworkMapper
+@implementation RNNetworkMapper {
+    NSMutableArray *connectedDevicesMutable;
+}
+
+-(instancetype) initWithDelegate: (id: RNNetworkMapperDelegate)delegate {
+    self = [super init];
+    if(self) {
+        self.delegate = delegate;
+        self.isScanRunning = NO;
+        self.lanScanner = [[MMLANScanner alloc] initWithDelegate:self];
+    }
+    
+    return self;
+}
 
 - (dispatch_queue_t)methodQueue
 {
     return dispatch_get_main_queue();
 }
+
 RCT_EXPORT_MODULE()
 
-- (NSString*) connectedDevices
-{
-    return @"Implement this method to return list of connected devices";
+- (void) startNetworkScan {
+    
+    connectedDevicesMutable = [[NSMutableArray alloc] init];
+ 
+    self.isScanRunning = YES;
+    
+    [self.lanscanner start];
+}
+
+-(void)stopNetworkScan {
+    
+    [self.lanScanner stop];
+    
+    self.isScanRunning=NO;
 }
 
 - (NSDictionary *)constantsToExport
 {
     return @{
              @"connectedDevices": self.connectedDevices,
+             @"isScanRunning": self.isScanRunning,
+             @"startNetworkScan": self.startNetworkScan,
+             @"stopNetworkScan": self.stopNetworkScan
              };
 }
 
+#pragma mark - MMLANScannerDelegate methods
+//The delegate methods of MMLANScanner
+-(void)lanScanDidFindNewDevice:(MMDevice*)device{
+    
+    //Check if the Device is already added
+    if (![connectedDevicesMutable containsObject:device]) {
+        
+        [connectedDevicesMutable addObject:device];
+    }
+    
+    NSSortDescriptor *valueDescriptor = [[NSSortDescriptor alloc] initWithKey:@"ipAddress" ascending:YES];
+    
+    
+    self.connectedDevices = [connectedDevicesMutable sortedArrayUsingDescriptors:@[valueDescriptor]];
+}
+
+-(void)lanScanProgressPinged:(float)pingedHosts from:(NSInteger)overallHosts {
+    
+    //Updating the progress value. MainVC will be notified by KVO
+    self.progressValue=pingedHosts/overallHosts;
+}
+
+-(void)lanScanDidFinishScanningWithStatus:(MMLanScannerStatus)status{
+    
+    self.isScanRunning=NO;
+}
+
 @end
-  
